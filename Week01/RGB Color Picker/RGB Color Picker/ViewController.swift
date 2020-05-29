0a2d40bdb40cb6c2b7329d1a1535a8dcd154fd8f
Andrew Lvovsky <alvov001@ucr.edu>
2020-05-28 22:58:15 -0700

    initialized new Xcode project

---
+++ Week01/RGB Color Picker/RGB Color Picker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RGB Color Picker
+//
+//  Created by Andrew Lvovsky on 5/28/20.
+//  Copyright © 2020 Andrew Lvovsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
