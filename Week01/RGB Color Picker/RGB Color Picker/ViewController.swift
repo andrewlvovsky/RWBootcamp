bcd693ac32192317f7ec86d2740a7d5e8774687e
Andrew Lvovsky <alvov001@ucr.edu>
2020-05-31 20:28:04 -0700

    added RW assets and trying to create stack views

--- Week01/RGB Color Picker/RGB Color Picker/ViewController.swift
+++ Week01/RGB Color Picker/RGB Color Picker/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        self.resetColor()
+        resetColor()
     }
     
     @IBAction func updateRedLabel() {
