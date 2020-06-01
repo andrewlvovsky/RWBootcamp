f6acde6308863e52785ebf2c588b610d7c90e273
Andrew Lvovsky <alvov001@ucr.edu>
2020-05-31 22:05:10 -0700

    updated dynamic label color change with more intuitive approach

--- Week01/RGB Color Picker/RGB Color Picker/ViewController.swift
+++ Week01/RGB Color Picker/RGB Color Picker/ViewController.swift
@@ -13,14 +13,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var redSlider : UISlider!
     @IBOutlet weak var greenSlider : UISlider!
     @IBOutlet weak var blueSlider : UISlider!
+    
     @IBOutlet weak var redLabel : UILabel!
     @IBOutlet weak var greenLabel : UILabel!
     @IBOutlet weak var blueLabel : UILabel!
     @IBOutlet weak var redValueLabel : UILabel!
     @IBOutlet weak var greenValueLabel : UILabel!
     @IBOutlet weak var blueValueLabel : UILabel!
-    
     @IBOutlet weak var colorNameLabel : UILabel!
+    
+    @IBOutlet weak var setColorButton : UIButton!
+    @IBOutlet weak var resetButton : UIButton!
      
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -48,11 +51,15 @@ class ViewController: UIViewController {
         let greenColor = CGFloat(greenSlider.value.rounded())/255
         let blueColor = CGFloat(blueSlider.value.rounded()) / 255
         
-        //let reverseColor = UIColor.init(displayP3Red: abs(redColor - 255), green: abs(greenColor - 255), blue: abs(blueColor - 255), alpha: 1.0)
-    
-        // Changes label colors dynamically based on background color so that labels aren't hard to see
-        redColor < 0.5 && greenColor < 0.5 && blueColor < 0.5 ? changeTextColor(value: 1) : changeTextColor(value: 0)
+        let reverseColor = UIColor.init(displayP3Red: abs(redColor - 1), green: abs(greenColor - 1), blue: abs(blueColor - 1), alpha: 1.0)
         
+        if ((redColor > 0.4 && redColor < 0.6) && (greenColor > 0.4 && greenColor < 0.6) && (blueColor > 0.4 && blueColor < 0.6)) {
+            // If color is gray-ish, just set label color to white to avoid gray-on-gray color
+            changeLabelColor(newColor: UIColor.init(displayP3Red: 1, green: 1, blue: 1, alpha: 1))   // basic white color
+        } else {
+            // Changes label colors dynamically based on background color so that labels aren't hard to see
+            changeLabelColor(newColor: reverseColor)
+        }
 
         let alert = UIAlertController(title: "Color", message: "Choose a name for your color.", preferredStyle: .alert)
         alert.addTextField(configurationHandler: nil)
@@ -74,24 +81,27 @@ class ViewController: UIViewController {
         redSlider.value = 0
         greenSlider.value = 0
         blueSlider.value = 0
-        
+
         redValueLabel.text = "0"
         greenValueLabel.text = "0"
         blueValueLabel.text = "0"
-        
         self.colorNameLabel.text = "Black"
         
         self.view.backgroundColor = UIColor.init(displayP3Red: 0, green: 0, blue: 0, alpha: 1.0)
+        changeLabelColor(newColor: UIColor.init(displayP3Red: 1, green: 1, blue: 1, alpha: 1.0))
     }
     
-    func changeTextColor(value: CGFloat) {
-        redLabel.textColor = UIColor.init(displayP3Red: value, green: value, blue: value, alpha: 1)
-        greenLabel.textColor = UIColor.init(displayP3Red: value, green: value, blue: value, alpha: 1)
-        blueLabel.textColor = UIColor.init(displayP3Red: value, green: value, blue: value, alpha: 1)
-        redValueLabel.textColor = UIColor.init(displayP3Red: value, green: value, blue: value, alpha: 1)
-        greenValueLabel.textColor = UIColor.init(displayP3Red: value, green: value, blue: value, alpha: 1)
-        blueValueLabel.textColor = UIColor.init(displayP3Red: value, green: value, blue: value, alpha: 1)
-        colorNameLabel.textColor = UIColor.init(displayP3Red: value, green: value, blue: value, alpha: 1)
+    func changeLabelColor(newColor: UIColor) {
+        redLabel.textColor = newColor
+        greenLabel.textColor = newColor
+        blueLabel.textColor = newColor
+        redValueLabel.textColor = newColor
+        greenValueLabel.textColor = newColor
+        blueValueLabel.textColor = newColor
+        colorNameLabel.textColor = newColor
+        
+        setColorButton.tintColor = newColor
+        resetButton.tintColor = newColor
     }
 
 }
