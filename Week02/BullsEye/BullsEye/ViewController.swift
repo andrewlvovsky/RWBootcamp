eaec99cb37f125ed7e9992f98f310f3b6f3e3883
Andrew Lvovsky <alvov001@ucr.edu>
2020-06-08 23:29:04 -0700

    separated game logic from controller and into model

--- Week02/BullsEye/BullsEye/ViewController.swift
+++ Week02/BullsEye/BullsEye/ViewController.swift
@@ -10,86 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  var currentValue = 0
-  var targetValue = 0
-  var score = 0
-  var round = 0
-  
+  //  var currentValue = 0
+  //  var targetValue = 0
+  //  var score = 0
+  //  var round = 0
+  var game = BullsEyeGame(currentValue: 0, targetValue: 0, score: 0, round: 0)
   @IBOutlet weak var slider: UISlider!
   @IBOutlet weak var targetLabel: UILabel!
   @IBOutlet weak var scoreLabel: UILabel!
   @IBOutlet weak var roundLabel: UILabel!
-  
   override func viewDidLoad() {
     super.viewDidLoad()
     let roundedValue = slider.value.rounded()
-    currentValue = Int(roundedValue)
-    startNewGame()
+    game.currentValue = Int(roundedValue)
+    game.startNewGame()
   }
 
   @IBAction func showAlert() {
-    
-    let difference = abs(targetValue - currentValue)
-    var points = 100 - difference
-    
-    score += points
-    
-    let title: String
-    if difference == 0 {
-      title = "Perfect!"
-      points += 100
-    } else if difference < 5 {
-      title = "You almost had it!"
-      if difference == 1 {
-        points += 50
-      }
-    } else if difference < 10 {
-      title = "Pretty good!"
-    } else {
-      title = "Not even close..."
-    }
-    
-    let message = "You scored \(points) points"
-    
+    let title, message: String
+    (title, message) = game.calculateScore()
     let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-    
-    let action = UIAlertAction(title: "OK", style: .default, handler: {
-      action in
-      self.startNewRound()
+    let action = UIAlertAction(title: "OK", style: .default, handler: { _ in
+      self.game.startNewRound()
+      self.updateView()
     })
-    
     alert.addAction(action)
-    
+
     present(alert, animated: true, completion: nil)
-    
+
   }
-  
+
   @IBAction func sliderMoved(_ slider: UISlider) {
     let roundedValue = slider.value.rounded()
-    currentValue = Int(roundedValue)
+    game.currentValue = Int(roundedValue)
   }
-  
-  func startNewRound() {
-    round += 1
-    targetValue = Int.random(in: 1...100)
-    currentValue = 50
-    slider.value = Float(currentValue)
-    updateLabels()
+
+  func updateView() {
+    slider.value = Float(game.currentValue)
+    targetLabel.text = String(game.targetValue)
+    scoreLabel.text = String(game.score)
+    roundLabel.text = String(game.round)
   }
-  
-  func updateLabels() {
-    targetLabel.text = String(targetValue)
-    scoreLabel.text = String(score)
-    roundLabel.text = String(round)
-  }
-  
+
   @IBAction func startNewGame() {
-    score = 0
-    round = 0
-    startNewRound()
+    game.startNewGame()
   }
-  
+
 }
-
-
-
