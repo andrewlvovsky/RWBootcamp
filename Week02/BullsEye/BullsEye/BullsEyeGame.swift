3dfaccdd194b6b553df48f3bdecd70161fc4d50f
Andrew Lvovsky <alvov001@ucr.edu>
2020-06-08 21:57:36 -0700

    started Week02 HW, created BullsEyeGame model

---
+++ Week02/BullsEye/BullsEye/BullsEyeGame.swift
@@ -0,0 +1,13 @@
+//
+//  BullsEyeGame.swift
+//  BullsEye
+//
+//  Created by Andrew Lvovsky on 6/8/20.
+//  Copyright © 2020 Ray Wenderlich. All rights reserved.
+//
+
+import Foundation
+
+struct BullsEyeGame{
+  
+}
