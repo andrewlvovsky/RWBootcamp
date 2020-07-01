373e7fb1d8fcfceec659b68aef12fff82ba2ec13
Andrew Lvovsky <alvov001@ucr.edu>
2020-07-01 14:21:10 -0400

    added Birdie starter and added two custom cell views

---
+++ Week05/Birdie-Final/ViewModel/ImageTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ImageTableViewCell.swift
+//  Birdie-Final
+//
+//  Created by Andrew Lvovsky on 7/1/20.
+//  Copyright © 2020 Jay Strawn. All rights reserved.
+//
+
+import UIKit
+
+class ImageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
