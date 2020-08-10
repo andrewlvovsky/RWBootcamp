2f14df506b95551bfa1318dd88fdfe43104437b0
Andrew Lvovsky <alvov001@ucr.edu>
2020-08-10 14:24:50 -0700

    added other animations and property animator

--- Week11/Animator/Animator/ViewController.swift
+++ Week11/Animator/Animator/ViewController.swift
@@ -16,28 +16,34 @@ class ViewController: UIViewController {
   @IBOutlet weak var scaleUpButton: UIButton!
   @IBOutlet weak var rotateRightButton: UIButton!
 
+  @IBOutlet weak var animationObject: UIView!
+
   @IBOutlet weak var roundConstraint: NSLayoutConstraint!
   @IBOutlet weak var scaleConstraint: NSLayoutConstraint!
   @IBOutlet weak var rotateConstraint: NSLayoutConstraint!
   @IBOutlet weak var playConstraint: NSLayoutConstraint!
 
   let buttonSize: CGFloat = 50
-  var menuToggled = false
+  var menuToggled = true
+  var currentScale: CGFloat = 1
+  var currentAngle: CGFloat = .pi
+  var animationDuration = 0
+  let animator = UIViewPropertyAnimator(duration: 2, curve: .easeInOut)
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+
+    // Hack to round buttons
     menuButton.layer.cornerRadius = menuButton.intrinsicContentSize.width / 2
     playButton.layer.cornerRadius = playButton.intrinsicContentSize.width / 2
     roundButton.layer.cornerRadius = roundButton.intrinsicContentSize.width / 2
     scaleUpButton.layer.cornerRadius = scaleUpButton.intrinsicContentSize.width / 2
     rotateRightButton.layer.cornerRadius = rotateRightButton.intrinsicContentSize.width / 2
 
-    self.rotateConstraint.constant = -buttonSize
-    self.scaleConstraint.constant = -buttonSize
-    self.roundConstraint.constant = -buttonSize
-    self.playConstraint.constant = -buttonSize
+    toggleMenu(self)
 
+    // Brings main menu button to the front of all the other views in the scene
     self.view.bringSubviewToFront(menuButton)
   }
 
@@ -47,7 +53,7 @@ class ViewController: UIViewController {
       delay: 0,
       usingSpringWithDamping: self.menuToggled ? 0.7 : 0.4,
       initialSpringVelocity: 0,
-      options: [],
+      options: [.allowUserInteraction],
       animations: {
         self.rotateConstraint.constant = self.menuToggled ? -self.buttonSize : self.buttonSize / 4
         self.scaleConstraint.constant = self.menuToggled ? -self.buttonSize : self.buttonSize / 4
@@ -56,8 +62,46 @@ class ViewController: UIViewController {
         self.view.layoutIfNeeded()
 
         self.menuToggled = !self.menuToggled
-    },
-      completion: nil)
+    }, completion: nil)
+  }
+
+  @IBAction func scaleUpAnimation(_ sender: Any) {
+    animator.addAnimations({
+      self.animationObject.transform = CGAffineTransform(
+        scaleX: self.currentScale * 1.5,
+        y: self.currentScale * 1.5)
+      self.currentScale = self.currentScale * 1.5
+      self.view.layoutIfNeeded()
+    }, delayFactor: 1)
+    print("Scale animation added to queue")
+  }
+
+  @IBAction func roundEdgeAnimation(_ sender: Any) {
+    animator.addAnimations({
+      self.animationObject.layer.cornerRadius = self.animationObject.frame.width / 2
+      self.view.layoutIfNeeded()
+    }, delayFactor: 1)
+    print("Round animation added to queue")
+  }
+
+  @IBAction func rotateAnimation(_ sender: Any) {
+    animator.addAnimations({
+      self.animationObject.transform = CGAffineTransform(
+        rotationAngle: self.currentAngle / 2
+      )
+      self.currentAngle = self.currentAngle * 2
+      self.view.layoutIfNeeded()
+    }, delayFactor: 1)
+    print("Rotate animation added to queue")
+  }
+
+  @IBAction func playAnimations(_ sender: Any) {
+    menuButton.isEnabled = false
+    if menuToggled {
+      toggleMenu(self)
+    }
+    animator.addCompletion { _ in self.menuButton.isEnabled = true}
+    animator.startAnimation()
   }
 }
 
