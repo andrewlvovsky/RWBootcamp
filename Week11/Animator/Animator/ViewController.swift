49847eb355590ee8309b3d3052387022d7924313
Andrew Lvovsky <alvov001@ucr.edu>
2020-08-10 09:35:41 -0700

    created new project for Animator

---
+++ Week11/Animator/Animator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Animator
+//
+//  Created by Andrew Lvovsky on 8/10/20.
+//  Copyright © 2020 Andrew Lvovsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
